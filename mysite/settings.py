4f4618ea1190914af9e6003a497d6426315d543e
Haaqu <alex.fiuk96@gmail.com>
2019-06-09 14:55:26 +0200

    changed to POSTGRESQL database

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'privateblog',
+        'USER': 'haqu',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
